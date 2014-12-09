5f95a73ff93668c087d0aa76480c563a761f5597
Vasily Semenov <semenov.vasily@gmail.com>
2014-12-08 22:33:20 -0500

    Conveyor implementation - to integrate/replace QueuedStorage

---
+++ Sandbox/Conveyor/Conveyor.cs
@@ -0,0 +1,275 @@
+﻿namespace Conveyor
+{
+	using System;
+	using System.Threading;
+
+	/// <summary>
+	/// Conveyor class. Allowed to queue values of type T and dequeue values of type V only after the item was processed
+	/// preserving the order of enqueued items. Allow to push, pop and process items in different threads.
+	/// T - type of values to Push in.
+	/// V - type of values to Pop out.
+	/// </summary>
+	public sealed class Conveyor<T, V>
+	{
+		#region Private Fields
+
+		/// <summary>
+		/// The locker to synchronize the threads.
+		/// </summary>
+		private object _processingLocker = new object ();
+
+		/// <summary>
+		/// The index of the last taken <see cref="Conveyor.ConveyorItem"/> for processing.
+		/// </summary>
+		private long _lastTakenForProcessingIndex;
+
+		/// <summary>
+		/// The index of the last processed <see cref="Conveyor.ConveyorItem"/>.
+		/// </summary>
+		private long _lastProcessedIndex;
+
+		/// <summary>
+		/// The index of the last added <see cref="Conveyor.ConveyorItem"/>.
+		/// </summary>
+		private long _lastAddedIndex;
+
+		/// <summary>
+		/// The first not poped item in <see cref="Conveyor.Conveyor"/> queue.
+		/// </summary>
+		private volatile ConveyorItem<T, V> _first;
+
+		/// <summary>
+		/// The last item in <see cref="Conveyor.Conveyor"/> queue.
+		/// </summary>
+		private volatile ConveyorItem<T, V> _last;
+
+		#endregion
+
+		#region Public Properties
+
+		/// <summary>
+		/// Gets the total amount of conveyed items.
+		/// </summary>
+		/// <value>The total amount if items being pushed.</value>
+		public long TotalConveyedItems {
+			get {
+				return _lastAddedIndex;
+			}
+		}
+
+		/// <summary>
+		/// Gets the total amount of processed items.
+		/// </summary>
+		/// <value>The total amount of items being processes.</value>
+		public long TotalProcessedItems {
+			get {
+				return _lastProcessedIndex;
+			}
+		}
+
+		/// <summary>
+		/// Gets the total items amount which are pending to be processed before pop them out.
+		/// </summary>
+		/// <value>The total amount of items to process.</value>
+		public long TotalItemsToProcess
+		{
+			get{
+				return _lastAddedIndex - _lastProcessedIndex;
+			}
+		}
+
+		#endregion
+
+		#region Constructor
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Conveyor.Conveyor"/> class.
+		/// </summary>
+		public Conveyor ()
+		{
+			_lastAddedIndex = _lastProcessedIndex = _lastTakenForProcessingIndex = 0;
+		}
+
+		#endregion
+
+		#region Public Interface
+
+		/// <summary>
+		/// Push the specified data item to the <see cref="Conveyor.Conveyor"/> queue.
+		/// </summary>
+		/// <param name="data">Data item to store in <see cref="Conveyor.Conveyor"/>.</param>
+		public void Push(T data)
+		{
+			lock (_processingLocker) {
+				ConveyorItem<T, V> item = new ConveyorItem<T, V> (data, _lastAddedIndex++);
+				if (_first == null) {
+					_first = item;
+					_last = item;
+					return;
+				}
+
+				_last.Next = item;
+				_last = _last.Next;
+			}
+		}
+
+		/// <summary>
+		/// Process the next available item with a specified processor and mark the item as Processed to allow to pop it out.
+		/// </summary>
+		/// <param name="processor">Processor to process the data.</param>
+		/// <returns><c>true</c> if if item was processed; otherwise, <c>false</c>.</returns>
+		/// <remarks>Throws an <see cref="System.ArgumentException"/> if processor was not specified.</remarks>
+		public bool Process(Func<T, V> processor)
+		{
+			if (_first == null)
+				return false;
+
+			if (processor == null)
+				throw new ArgumentException ("Processor should be specified to process conveyed item.");
+
+			ConveyorItem<T, V> item = null;
+
+			lock (_processingLocker) {
+				if (_first == null)
+					return false;
+
+				item = _first [_lastTakenForProcessingIndex];
+				if (item == null)
+					return false;
+
+				_lastTakenForProcessingIndex++;
+			}
+
+			item.ProcessedValue = processor (item.Data);
+			Interlocked.Increment(ref _lastProcessedIndex);
+
+			return true;
+		}
+
+		/// <summary>
+		/// Pop the next available processed item.
+		/// </summary>
+		/// <param name="timeout">Amount of time (in msecs) after that the default(V) will be returned.</param>
+		/// <returns>Processed value of type V or <c>default(V)</c> if timeout exeeded and there is no processed 
+		/// item available in <see cref="Conveyor.Conveyor"/>.</returns>
+		public V Pop(int timeout)
+		{
+			int waitingTime = 0;
+
+			while (_first == null || !_first.IsProcessed) {
+				if (timeout != Timeout.Infinite && waitingTime == timeout)
+					return default(V);
+
+				waitingTime++;
+				Thread.Sleep (1);
+			}
+
+			lock (_processingLocker) {
+				if (_first == null)
+					return default(V);
+
+				var item = _first;
+				_first = _first.Next;
+				return item.ProcessedValue;
+			}
+		}
+
+		/// <summary>
+		/// Clear the <see cref="Conveyor.Conveyor"/> queue.
+		/// </summary>
+		public void Clear()
+		{
+			lock (_processingLocker) {
+				_first = null;
+				_last = null;
+			}
+		}
+
+		#endregion
+	}
+
+	/// <summary>
+	/// Conveyor item class. Container used to store data internally in <see cref="Conveyor.ConveyorItem"/>.
+	/// </summary>
+	internal sealed class ConveyorItem<T, V>
+	{
+		#region Public Properties
+
+		/// <summary>
+		/// Gets the data item stored in container.
+		/// </summary>
+		internal T Data {
+			get;
+			private set;
+		}
+
+		/// <summary>
+		/// Gets the index if conveyed item.
+		/// </summary>
+		internal long Index {
+			get;
+			private set;
+		}
+
+		/// <summary>
+		/// Gets or sets the result of data processing.
+		/// </summary>
+		/// <value>The processed value.</value>
+		internal V ProcessedValue{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets or sets the followed item in  <see cref="Conveyor.ConveyorItem"/>.
+		/// </summary>
+		internal ConveyorItem<T, V> Next {
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets a flag indicating whether this data is processed.
+		/// </summary>
+		/// <value><c>true</c> if this data is processed; otherwise, <c>false</c>.</value>
+		internal bool IsProcessed {
+			get{ return ProcessedValue != null; }
+		}
+
+		/// <summary>
+		/// Gets one of the following <see cref="Conveyor.ConveyorItem"/> with the specified idx.
+		/// Returns <c>null</c> if there is no following item with the specified index.
+		/// </summary>
+		/// <param name="idx">Index of the following item.</param>
+		internal ConveyorItem<T, V> this [long idx] {
+			get {
+				var item = this;
+				while (item != null)
+					if (item.Index == idx)
+						return item;
+					else
+						item = item.Next;
+
+				return null;
+			}
+		}
+
+		#endregion
+
+		#region Constructor
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Conveyor.ConveyorItem"/> class.
+		/// </summary>
+		/// <param name="data">Data to storee in <see cref="Conveyor.ConveyorItem"/>.</param>
+		/// <param name="index">Index of the item in a queue.</param>
+		internal ConveyorItem(T data, long index)
+		{
+			Data = data;
+			Index = index;
+		}
+
+		#endregion
+	}
+}
+
---
+++ Sandbox/Conveyor/Program.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Conveyor
+{
+	class MainClass
+	{
+		private static Conveyor<int[], string> c = new Conveyor<int[], string> ();
+		private const int MAX_DELAY = 100;
+		private const bool VERBOSE = true;
+
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("VERBOSE mode {0}", (VERBOSE ? "On" : "Off"));
+
+			singleThreadTest ();
+			Console.WriteLine ("Please hit [Enter] to run next test");
+			Console.ReadLine ();
+			multiThreadTest ();
+			Console.WriteLine ("Please hit [Enter] to run next test");
+			Console.ReadLine ();
+			multiThreadRealTest ();
+		}
+
+		private static void singleThreadTest()
+		{
+			int idx = 0;
+			Console.WriteLine ("Single Thread Test");
+
+			c.Clear ();
+
+			c.Push (new int[]{ idx++ });
+			c.Push (new int[]{ idx++ });
+			c.Push (new int[]{ idx++ });
+
+			c.Process ((d) => {
+				return d[0].ToString ();
+			});
+
+			c.Process ((d) => {
+				return d[0].ToString ();
+			});
+
+			Console.WriteLine (c.Pop (MAX_DELAY));
+			Console.WriteLine (c.Pop (MAX_DELAY));
+
+			c.Process ((d) => {
+				return d[0].ToString ();
+			});
+
+			Console.WriteLine (c.Pop (MAX_DELAY));
+
+
+			Console.WriteLine ("Now the value should be empty because there were only 3 items conveyed. Value: '{0}'", c.Pop (MAX_DELAY));
+			Console.WriteLine ("Single Thread Test Complete");
+		}
+
+		private static void multiThreadTest()
+		{
+			int MAX_OBJECTS = 100;
+			int PROCESSING_THREADS = 4;
+
+			int idx = 0;
+			Random random = new Random ();
+			List<string> original = new List<string> ();
+			List<string> result = new List<string> ();
+			Thread[] threads = new Thread[PROCESSING_THREADS];
+			CountdownEvent cdevent = new CountdownEvent (PROCESSING_THREADS);
+
+			Console.WriteLine ("Multi Thread Test");
+			Console.WriteLine ("Going to push {0} items and process/pop them in {1} threads.", MAX_OBJECTS, PROCESSING_THREADS);
+			c.Clear ();
+
+			while (idx < MAX_OBJECTS) {
+				original.Add (idx.ToString ());
+				c.Push (new int[]{ idx++ });
+			}
+
+			Func<bool> func = () => {
+				while (c.TotalItemsToProcess > 0) {
+					c.Process ((d) => {
+						Thread.Sleep(random.Next(MAX_DELAY));
+						return d [0].ToString ();
+					});
+
+					lock(c)
+					{
+						var val = c.Pop(MAX_DELAY);
+
+						if(!VERBOSE)
+							Console.WriteLine ("[{0}] {1}", Thread.CurrentThread.Name, val);
+						if(val != default(string))
+							result.Add(val);
+					}
+				}
+				cdevent.Signal();
+				return true;
+			};
+
+			for (int ii = 0; ii < PROCESSING_THREADS; ii++) {
+				threads [ii] = new Thread (() => {
+					func ();
+				});
+				threads [ii].Name = "T" + (ii + 1).ToString ();
+				threads [ii].IsBackground = true;
+				threads [ii].Start ();
+			}
+
+			cdevent.Wait ();
+
+			Console.WriteLine ("Multi Thread Test Complete. Validating.");
+
+			bool valid = true;
+			if (original.Count != result.Count) {
+				valid = false;
+				Console.WriteLine ("Validation failed: count is invalid.");
+			}
+			else {
+				for (int ii = 0; ii < original.Count; ii++)
+					if (original [ii] != result [ii]) {
+						Console.WriteLine ("Validation failed: sequence does not match.");
+						valid = false;
+						break;
+					}
+			}
+
+			if(valid)
+				Console.WriteLine ("Validation succeed. Count and order of pushed and poped items is equal.");
+
+		}
+
+		private static void multiThreadRealTest()
+		{
+			int MAX_OBJECTS = 10000;
+			int PROCESSING_THREADS = 8;
+			int READING_THREADS = 4;
+			int WRINTING_THREADS = 4;
+
+			int idx = 0;
+			Random random = new Random ();
+			Stopwatch sw = new Stopwatch ();
+			List<string> original = new List<string> ();
+			List<string> result = new List<string> ();
+			Thread[] proessingThreads = new Thread[PROCESSING_THREADS];
+			Thread[] readingThreads = new Thread[READING_THREADS];
+			Thread[] wrintingThreads = new Thread[WRINTING_THREADS];
+
+			CountdownEvent cdevent = new CountdownEvent (READING_THREADS + PROCESSING_THREADS + WRINTING_THREADS);
+
+			Console.WriteLine ("Real Multi Thread Test");
+			Console.WriteLine ("Going to push {0} items in {1} thread(s), process them in {2} thread(s) and then pop out in {3} thread(s).",
+				MAX_OBJECTS, WRINTING_THREADS, PROCESSING_THREADS, READING_THREADS);
+
+			Console.WriteLine ("It may take up to {0} sec due to random delays added for simulation.",
+				MAX_DELAY * MAX_OBJECTS * 3 / 1000);
+			c.Clear ();
+
+			Func<bool> writingFunc = () => {
+				Console.WriteLine (string.Format("Starting [{0}] thread to PUSH the data.", Thread.CurrentThread.Name));
+				while (idx < MAX_OBJECTS) {
+					lock(random)
+					{
+						original.Add (idx.ToString ());
+						c.Push (new int[]{ idx++ });
+						if(!VERBOSE)
+							Console.WriteLine ("[{0}] {1}", Thread.CurrentThread.Name, idx - 1);
+					}
+					Thread.Sleep(random.Next(MAX_DELAY));
+				}
+				Console.WriteLine ("Thread [{0}] complete.", Thread.CurrentThread.Name);
+				cdevent.Signal();
+				return true;
+			};
+
+			Func<bool> processfunc = () => {
+				while(idx == 0)
+					Thread.Sleep(1); //not yet started to populate
+
+				Console.WriteLine (string.Format("Starting [{0}] thread to PROCESS the data.", Thread.CurrentThread.Name));
+				while(c.TotalItemsToProcess > 0 || idx < MAX_OBJECTS) {
+					c.Process ((d) => {
+						if(!VERBOSE)
+							Console.WriteLine ("[{0}] {1}", Thread.CurrentThread.Name, d[0]);
+						Thread.Sleep(random.Next(MAX_DELAY));
+						return d [0].ToString ();
+					});
+				}
+				cdevent.Signal();
+				Console.WriteLine ("Thread [{0}] complete.", Thread.CurrentThread.Name);
+				return true;
+			};
+
+			Func<bool> readFunc = () => {
+				while(idx == 0)
+					Thread.Sleep(1); //not yet started to populate
+
+				Console.WriteLine ("Starting [{0}] thread to POP the data.", Thread.CurrentThread.Name);
+				while(c.TotalItemsToProcess > 0 || idx < MAX_OBJECTS ) {
+					lock(c)
+					{
+						var val = c.Pop(MAX_DELAY);
+
+						if(!VERBOSE)
+							Console.WriteLine ("[{0}] {1}", Thread.CurrentThread.Name, val);
+						if(val != default(string))
+							result.Add(val);
+					}
+				}
+				Console.WriteLine ("Thread [{0}] complete.", Thread.CurrentThread.Name);
+				cdevent.Signal();
+				return true;
+			};
+
+			sw.Start ();
+			for (int ii = 0; ii < WRINTING_THREADS; ii++) {
+				wrintingThreads [ii] = new Thread (() => {
+					writingFunc ();
+				});
+				wrintingThreads [ii].Name = "W" + (ii + 1).ToString ();
+				wrintingThreads [ii].IsBackground = true;
+				wrintingThreads [ii].Start ();
+			}
+
+			for (int ii = 0; ii < PROCESSING_THREADS; ii++) {
+				proessingThreads [ii] = new Thread (() => {
+					processfunc ();
+				});
+				proessingThreads [ii].Name = "P" + (ii + 1).ToString ();
+				proessingThreads [ii].IsBackground = true;
+				proessingThreads [ii].Start ();
+			}
+
+			for (int ii = 0; ii < READING_THREADS; ii++) {
+				readingThreads [ii] = new Thread (() => {
+					readFunc ();
+				});
+				readingThreads [ii].Name = "R" + (ii + 1).ToString ();
+				readingThreads [ii].IsBackground = true;
+				readingThreads [ii].Start ();
+			}
+
+			cdevent.Wait ();
+			sw.Stop ();
+
+			Console.WriteLine ("Real Multi Thread Test Complete in {0} sec. Validating.", sw.ElapsedMilliseconds / 1000);
+
+			bool valid = true;
+			if (original.Count != result.Count) {
+				valid = false;
+				Console.WriteLine ("Validation failed: count is invalid.");
+			}
+			else {
+				for (int ii = 0; ii < original.Count; ii++)
+					if (original [ii] != result [ii]) {
+						Console.WriteLine ("Validation failed: order does not match.");
+						valid = false;
+						break;
+					}
+			}
+
+			if(valid)
+				Console.WriteLine ("Validation succeed. Count and order of pushed and poped items is equal.");
+
+		}
+	}
+}
